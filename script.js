feac7b75d7a491326a146ee1360fbd48f75fa5e7
Yu Yang <yu.yang@cittacorp.com>
2017-05-12 00:32:02 -0400

    disable hove effect for picture

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 "use strict";
 
-(function () {
+(function() {
 
-	$("#carousel-example-generic").hide();
+    $("#carousel-example-generic").hide();
 
-	$( "#logo" ).hover(function(){
-	    $(this).filter(':not(:animated)').effect( "shake" );
-	});
+    $("#logo").hover(function() {
+        $(this).filter(':not(:animated)').effect("shake");
+    });
 
-	$( "#profile_photo" ).hover(function(){
-	    $(this).filter(':not(:animated)').animate({ width: "250px" });
-	}, function() {
-	    $(this).animate({ width: "200px" });
-	});
+    // $( "#profile_photo" ).hover(function(){
+    //     $(this).filter(':not(:animated)').animate({ width: "250px" });
+    // }, function() {
+    //     $(this).animate({ width: "200px" });
+    // });
 
-	$( "#aboutme" ).click(function(){
-			$(".info").show();
-	    $("#carousel-example-generic").hide();
-	});
+    $("#aboutme").click(function() {
+        $(".info").show();
+        $("#carousel-example-generic").hide();
+    });
 
-	$( "#portfolio" ).click(function(){
-			$(".info").hide();
-	    $("#carousel-example-generic").show();
-	});
+    $("#portfolio").click(function() {
+        $(".info").hide();
+        $("#carousel-example-generic").show();
+    });
 
 })();
